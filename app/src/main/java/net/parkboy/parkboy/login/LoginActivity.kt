d6e63bfe48f3261c515d307f526f2aebe083b62d
matthew <>
2019-02-17 18:26:38 +0700

    Add Login view

---
+++ app/src/main/java/net/parkboy/parkboy/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package net.parkboy.parkboy.login
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import net.parkboy.parkboy.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.view_login)
+    }
+}
